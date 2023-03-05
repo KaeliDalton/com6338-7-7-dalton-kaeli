b60cd71df0767456fa3849e71f117084b02d071e
Kaeli Dalton <Kaelidalton@ufl.edu>
2023-03-05 14:49:33 -0500

    run quiz and move questions forward

--- index.js
+++ index.js
@@ -85,6 +85,30 @@ var questionsArr = [
     quiz.appendChild(answerBtn)
     }
     
+    function runQuiz(){
+        //fresh timer per question
+        seconds = 30
+        quiz.innerHTML = ''
+        // question
+         var questions = questionsArr[i]
+         question.textContent = questions.question
+         quiz.appendChild(question)
+       
+         //buttons
+         var choices = document.createElement('div')
+         choices.id = 'choices'
+         quiz.appendChild(choices)
+         questions.options.forEach(function(choice){
+           var optionBtn = document.createElement('button')
+           optionBtn.textContent = choice
+           choices.appendChild(optionBtn)
+         })
+         //timer
+         timer.id = "timer"
+         timer.textContent = seconds
+         quiz.appendChild(timer)
+         countdown()
+       }
       
        //countdown
       function countdown(){
@@ -105,5 +129,25 @@ var questionsArr = [
         }
       }, 1000)
       }
-      
-     
+
+      quiz.onclick = function(e){
+        //start quiz
+        if(e.target.id === 'start-quiz'){
+          runQuiz()
+        }else if (e.target.parentElement.id === 'choices' && e.target.tagName === 'BUTTON'){
+          //move forward if choice made
+          if(e.target.textContent === questionsArr[i].answer){
+            //add to score if correct
+            numCorrect++
+          }
+          //reset for new question
+          clearInterval(timerEl)
+          i++
+          //determine if move forward or end quiz
+          if(i < questionsArr.length){
+            runQuiz()
+          }else{
+            endQuiz()
+          }
+        }
+    }
